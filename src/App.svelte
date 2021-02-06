3b28179dff2b924d27106179a4932087fd08f039
daichi sekiguchi <daichi.tennis.928@icloud.com>
2021-02-06 21:45:37 +0900

    init

---
+++ src/App.svelte
@@ -0,0 +1,39 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<h1>こんにちは。関口大地です</h1>
+	<div class="block">
+    <p>Software Enginner頑張ってます</p>
+    <p>ひととなりは<a href="https://note.com/smartcamp_tent/n/nbe612500f39a">こんな感じ</a></p>
+    <p><span>github:</span><a href="https://github.com/daichi1998928">見てね</a></p>
+    <p><span>note:</span><a href="https://note.com/daichi1998928">読んでね</a></p>
+    <p><span>Medium:</span><a href="https://daichi-tennis-928.medium.com/">こっちも読んでね</a></p>
+    <p>好きな技術</p>
+    <p>Ruby,Golang,AWS(ECS,CodeDeploy)</p>
+    <p>サーバーサイドやクラウドインフラに興味があります。<br>技術の話も好きですがプロダクトづくりの話も大好きです。</p>
+  </div>
+</main>
+
+<style>
+	main {
+		text-align: left;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 1.2em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
